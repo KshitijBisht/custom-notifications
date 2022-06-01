a972ea8401d6e73b9cef4b4d27eeb562e46602d4
Kshitij Bisht <bishtkshitij@protonmail.com>
2022-06-01 23:20:17 +0530

    added notification container

--- customNotification.js
+++ customNotification.js
@@ -9,11 +9,18 @@ template.innerHTML = `
     }
     .notify-container {
         position: absolute;
-        bottom: 500%;
+        bottom: 800%;
         z-index: 9;
         width: 500px;
         background: white;
         box-shadow: 5px 5px 10px rgba(0,0,0,.1);
+        font-size: .8em;
+        color: white;
+        border-radius: .5em;
+        padding: 1em;
+        transform: scale(0);
+        transform-origin: bottom ;
+        transition: transform .5s cubic-bezier(0.860,0.000,0.070,1.000)
     }
 
     </style>
@@ -24,7 +31,7 @@ template.innerHTML = `
         <button id="info" class="info">Info</button>
 
         <div class="notify-container">
-            My custom Notifications
+            <slot name="message"/>
         </div>    
     </div>
 
@@ -37,6 +44,37 @@ class CustomNotification extends HTMLElement {
         super();
         this.attachShadow({mode: 'open'});
         this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+    notification(expandState){
+        const notification = this.shadowRoot.querySelector('.notify-container')
+        const success = this.shadowRoot.querySelector('.success')
+        const error = this.shadowRoot.querySelector('.error')
+        const info = this.shadowRoot.querySelector('.info')
+
+        if(expandState === true){
+            notification.style.transform = 'scale(1)';
+            expandState = false
+        }
+        else {
+            notification.style.transform = 'scale(0)';
+
+        }
+
+
+
+    }
+    // do something once dom is loaded
+    connectedCallback(){
+        this.shadowRoot.querySelector('.success').addEventListener('click', ()=> {
+            this.notification(true)
+        })
+        this.shadowRoot.querySelector('.error').addEventListener('click', ()=> {
+            this.notification(false)
+        })
+
+        if(this.getAttribute('notification_background')){
+            this.shadowRoot.querySelector('.notify-container').style.background = this.getAttribute('notification_background');
+        }
     }
 }
 
