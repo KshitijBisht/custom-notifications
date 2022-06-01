fba432c0bfa32a5add6aaf42ee50787301c73036
Kshitij Bisht <bishtkshitij@protonmail.com>
2022-06-02 01:06:52 +0530

    custom notifications completed

--- customNotification.js
+++ customNotification.js
@@ -2,7 +2,7 @@ const template = document.createElement('template')
 template.innerHTML = `
 
     <style>
-    .tooltip-container {
+    .container {
         display: inline-block;
         position: relative;
         z-index: 2;
@@ -18,20 +18,14 @@ template.innerHTML = `
         color: white;
         border-radius: .5em;
         padding: 1em;
-        transform: scale(0);
-        transform-origin: bottom ;
-        transition: transform .5s cubic-bezier(0.860,0.000,0.070,1.000)
     }
 
     </style>
 
-    <div class="tooltip-container">
-        <button id="success" class="success">Success</button>
-        <button id="error" class="error">Error</button>
-        <button id="info" class="info">Info</button>
-
+    <div class="container">
         <div class="notify-container">
-            <slot name="message"/>
+            <h3 class="notify-title"></h3>
+            <p class="notify-content"> </p>
         </div>    
     </div>
 
@@ -45,36 +39,29 @@ class CustomNotification extends HTMLElement {
         this.attachShadow({mode: 'open'});
         this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
-    notification(expandState){
-        const notification = this.shadowRoot.querySelector('.notify-container')
-        const success = this.shadowRoot.querySelector('.success')
-        const error = this.shadowRoot.querySelector('.error')
-        const info = this.shadowRoot.querySelector('.info')
-
-        if(expandState === true){
-            notification.style.transform = 'scale(1)';
-            expandState = false
-        }
-        else {
-            notification.style.transform = 'scale(0)';
-
-        }
-
-
-
-    }
     // do something once dom is loaded
     connectedCallback(){
-        this.shadowRoot.querySelector('.success').addEventListener('click', ()=> {
-            this.notification(true)
-        })
-        this.shadowRoot.querySelector('.error').addEventListener('click', ()=> {
-            this.notification(false)
-        })
+        const notificationType = this.getAttribute('id');
+        const notifyTitle = this.shadowRoot.querySelector('.notify-title');
+        const notifyContent = this.shadowRoot.querySelector('.notify-content');
+        const notifyContainer = this.shadowRoot.querySelector('.notify-container')
 
-        if(this.getAttribute('notification_background')){
-            this.shadowRoot.querySelector('.notify-container').style.background = this.getAttribute('notification_background');
+        if(notificationType && notificationType === 'error'){
+            notifyTitle.innerHTML = 'Error'
+            notifyContent.innerHTML = 'Error in Operation'
+            notifyContainer.style.background = "#bd0909"
         }
+        else if(notificationType && notificationType  === 'success'){
+            notifyTitle.innerHTML = 'Success'
+            notifyContent.innerHTML = 'Success in Operation'
+            notifyContainer.style.background = "green"
+        }
+        else if(notificationType && notificationType  === 'info'){
+            notifyTitle.innerHTML = 'Info'
+            notifyContent.innerHTML = 'This was the information'
+            notifyContainer.style.background = "orange"
+        }
+        
     }
 }
 
